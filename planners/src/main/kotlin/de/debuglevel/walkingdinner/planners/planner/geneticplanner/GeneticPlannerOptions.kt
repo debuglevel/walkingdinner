b40b8e880fa46c184c9ec1efadd87aba3b6a113b
Marc Kohaupt <debuglevel@gmail.com>
2021-11-08 21:05:46 +0100

    Docs

--- planners/src/main/kotlin/de/debuglevel/walkingdinner/planners/planner/geneticplanner/GeneticPlannerOptions.kt
+++ planners/src/main/kotlin/de/debuglevel/walkingdinner/planners/planner/geneticplanner/GeneticPlannerOptions.kt
@@ -7,8 +7,17 @@ import java.util.function.Consumer
 
 data class GeneticPlannerOptions(
     val teams: List<Team>,
+    /**
+     * Fitness level to beat (minimization problem, i.e. fitness must be less than this threshold)
+     */
     val fitnessThreshold: Double,
+    /**
+     * Number of generations with constant fitness level to stop further calculations
+     */
     val steadyFitness: Int,
+    /**
+     * Size of the population
+     */
     val populationsSize: Int,
     var evolutionResultConsumer: Consumer<EvolutionResult<EnumGene<Team>, Double>>? = null
 )
