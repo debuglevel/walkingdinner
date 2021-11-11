4e56f284bfec90bd472e07e6252a1a71477000e3
Marc Kohaupt <debuglevel@gmail.com>
2021-11-11 22:54:46 +0100

    Fix update issues with newer Angular

--- web-angular/src/app/settings.http.service.ts
+++ web-angular/src/app/settings.http.service.ts
@@ -4,23 +4,22 @@ import { SettingsService } from "./settings.service";
 import { Settings } from "./settings";
 
 // see: https://medium.com/faun/angular-on-docker-environment-specific-settings-1e92c3ad01e6
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class SettingsHttpService {
+  constructor(
+    private http: HttpClient,
+    private settingsService: SettingsService
+  ) {}
 
-    constructor(private http: HttpClient, private settingsService: SettingsService) {
-    }
-
-    initializeApp(): Promise<any> {
-        return new Promise(
-            (resolve) => {
-                this.http.get('assets/settings.json')
-                    .toPromise()
-                    .then(response => {
-                            this.settingsService.settings = <Settings>response;
-                            resolve();
-                        }
-                    )
-            }
-        );
-    }
+  initializeApp(): Promise<any> {
+    return new Promise<void>((resolve) => {
+      this.http
+        .get("assets/settings.json")
+        .toPromise()
+        .then((response) => {
+          this.settingsService.settings = <Settings>response;
+          resolve();
+        });
+    });
+  }
 }
