1b8982c600f95a00070417ee26ccf4d086a8305b
Marc Kohaupt <debuglevel@gmail.com>
2021-11-16 21:32:42 +0100

    Docs

--- backend/src/main/kotlin/de/debuglevel/walkingdinner/backend/team/importer/TeamImporter.kt
+++ backend/src/main/kotlin/de/debuglevel/walkingdinner/backend/team/importer/TeamImporter.kt
@@ -2,6 +2,12 @@ package de.debuglevel.walkingdinner.backend.team.importer
 
 import de.debuglevel.walkingdinner.backend.team.Team
 
+/**
+ * Imports [Team]s.
+ */
 interface TeamImporter {
+    /**
+     * Imports the [Team]s.
+     */
     fun import(): List<Team>
 }
--- backend/src/main/kotlin/de/debuglevel/walkingdinner/backend/team/importer/csv/CsvTeamImporter.kt
+++ backend/src/main/kotlin/de/debuglevel/walkingdinner/backend/team/importer/csv/CsvTeamImporter.kt
@@ -5,20 +5,26 @@ import de.debuglevel.walkingdinner.backend.team.Team
 import de.debuglevel.walkingdinner.backend.team.importer.TeamImporter
 import mu.KotlinLogging
 
+/**
+ * Imports [Team]s from a [csv] string.
+ */
 class CsvTeamImporter(private val csv: String) : TeamImporter {
     private val logger = KotlinLogging.logger {}
 
     override fun import(): List<Team> {
-        logger.debug("Importing teams from CSV...")
+        logger.debug("Importing teams...")
 
         val teams = getTeamDTOs()
             //.onEach { logger.debug("Imported team: address '${it.address}', city '${it.city}'") }
             .map { it.toTeam() }
 
-        logger.debug("Imported ${teams.count()} teams from CSV")
+        logger.debug("Imported ${teams.count()} teams")
         return teams
     }
 
+    /**
+     * Get [TeamDTO]s from the CSV file.
+     */
     private fun getTeamDTOs(): List<TeamDTO> {
         return csv.reader().use { reader ->
             try {
